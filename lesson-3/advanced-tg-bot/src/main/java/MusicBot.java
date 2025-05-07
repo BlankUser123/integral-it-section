f3f0e6ea06b96c37518ddfeb7f8ba49c1cb18196
tkrxva123 <tkrxvaktn@gmail.com>
2025-05-07 20:17:06 +0300

    feat: added update method

--- lesson-3/advanced-tg-bot/src/main/java/MusicBot.java
+++ lesson-3/advanced-tg-bot/src/main/java/MusicBot.java
@@ -1,11 +1,21 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.List;
+
 public class MusicBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String msg = update.getMessage().getText();
+            if(msg.startsWith("/похоже")){
+                String query = msg.replace("/похоже", "").trim();
+                List<String> results = MusicRecommender.getRecommendations(query);
+                String reply = "🎵 Похожие артисты:\n" + String.join("\n", results);
+                sendMessage(update.getMessage().getChatId(),reply);
+            }
+        }
     }
 
     @Override
